f9ef5e56b1777b28b877e0f5e35a84d32e419bb4
Tom Cebula <MindlessShad@gmail.com>
2016-11-13 17:37:36 +0000

    Changed starting city so that it is random. Added a limit on the enhanced loop, saving some time

--- Coursework/src/Travelling_Salesman/MyAlgorithm.java
+++ Coursework/src/Travelling_Salesman/MyAlgorithm.java
@@ -2,7 +2,7 @@ package Travelling_Salesman;
 
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Random;
 
 public class MyAlgorithm {
 	
@@ -11,13 +11,25 @@ public class MyAlgorithm {
 		return Point2D.distance(currentCity.getX(), currentCity.getY(), possibleCity.getX(), possibleCity.getY());
 	}
 	
+	public static int randInt(int min, int max) {
+
+	    // Usually this can be a field rather than a method variable
+	    Random rand = new Random();
+
+	    // nextInt is normally exclusive of the top value,
+	    // so add 1 to make it inclusive
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+	    return randomNum;
+	}
 
 	public ArrayList<Point2D> algorithm(ArrayList<Point2D> cities){
 		
 		ArrayList<Point2D> result = new ArrayList<Point2D>();
 		Point2D closest = null;
 		//Do not remove the first city in list, will cause the algorithm to skip the final city in result
-		Point2D currentCity = cities.get(0);
+		//This will ensure a random city start, might improve the route length
+		Point2D currentCity = cities.get(randInt(0, cities.size()-1));
 		
 		while(cities.size() > 0) {
 			
@@ -32,6 +44,10 @@ public class MyAlgorithm {
 				if(getDistance(currentCity, city) < distance) {
 					closest = city;
 					distance = getDistance(currentCity, city);
+					//I'm putting a limit on the distance as assumption that not many distances will be smaller than 50 so I can save some time by breaking the loop when first value lower than 50 is encountered 
+					if (distance < 50){
+						break;
+					}
 				}
 			}
 			
